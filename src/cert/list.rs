3dc3930848ee54f1f32cb8edf5e77a2b6c861853
Travis Athougies <travis@athougies.net>
2026-08-27 15:32:19 +0000

    cert list

--- src/cert/list.rs
+++ src/cert/list.rs
@@ -1,19 +1,42 @@
 use std::error::Error;
 
+use crate::pkiboo::Cert;
+use crate::ui::ListView;
+use crate::util::Name;
+
 #[derive(clap::Args)]
 pub struct Args {
     /// Only show certificates issued by this certificate
     #[arg(long)]
-    by: Option<String>,
+    by: Option<Name<Cert>>,
 
     #[command(flatten)]
     list_options: crate::util::ListOptions,
 }
 
 pub async fn main<Ui: crate::Ui>(
-    _boo: &crate::PkiBoo<Ui>,
+    boo: &crate::PkiBoo<Ui>,
     _cert: &super::Args,
-    _args: &Args,
+    args: &Args,
 ) -> Result<(), Box<dyn Error>> {
-    todo!()
+    let db = boo.open_database()?;
+
+    let certificates = db
+        .certs
+        .iter()
+        .filter(|certificate| {
+            args.by
+                .as_ref()
+                .is_none_or(|issuer| certificate.issuer.as_ref() == Some(issuer))
+        })
+        .cloned()
+        .collect::<Vec<_>>();
+
+    boo.ui()
+        .list(certificates)
+        .with_options(&args.list_options)
+        .display()
+        .await;
+
+    Ok(())
 }
